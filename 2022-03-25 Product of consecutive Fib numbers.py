e318dd5a9b39de9edf4d6e5833bcd05d2d19c889
SzybkiRabarbar <maksjoni36@gmail.com>
2022-03-25 21:11:16 +0000

    Wysłane automatycznie

--- 2022-03-25 Product of consecutive Fib numbers.py
+++
@@ -1,5 +0,0 @@
-def productFib(prod):
-    fibon = [0,1,1,2]
-    for i in range(20):
-        fibon.append(fibon[-1]+fibon[-2])
-    return fibon
---
+++ 2022-03-25Product of consecutive Fib numbers.py
@@ -0,0 +1,17 @@
+'''
+https://www.codewars.com/kata/5541f58a944b85ce6d00006a/python
+'''
+
+def productFib(prod):
+    fibon = [0,1]
+    for i, amongus in enumerate(fibon):
+        fibon.append(fibon[-1]+fibon[-2])
+        if fibon[i] * fibon[i+1] == prod:
+            content = [fibon[i], fibon[i+1], True]
+            return content
+        elif (fibon[i] * fibon[i+1]) > prod:
+            content = [fibon[i], fibon[i+1], False]
+            return content
+
+
+print(productFib(4895))
