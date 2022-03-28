c21ad9c194dc07994ef0076a1e73162704c7f5b1
SzybkiRabarbar <maksjoni36@gmail.com>
2022-03-28 23:44:26 +0000

    "Wysłane automatycznie"

--- 2022-03-29Create Phone Number.py
+++ 2022-03-29Create Phone Number.py
@@ -1,5 +1,7 @@
 '''
-def create_phone_number(n):
+https://www.codewars.com/kata/525f50e3b73515a6db000b83/python
+'''
+def create_phone_number1(n):
     first =str()
     seccond =str()
     three =str()
@@ -13,8 +15,9 @@ def create_phone_number(n):
             three += str(num)
 
     return '('+first+') '+seccond+'-'+three
-'''
-def create_phone_number(n):
+
+def create_phone_number2(n):
     return f"({n[0]}{n[1]}{n[2]}) {n[3]}{n[4]}{n[5]}-{n[6]}{n[7]}{n[8]}{n[9]}"
 
-print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+print(create_phone_number1([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+print(create_phone_number2([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
