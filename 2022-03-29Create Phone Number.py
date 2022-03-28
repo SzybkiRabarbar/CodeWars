ed65eedc3ac06fd85a6bad177968227d05a4e14e
SzybkiRabarbar <maksjoni36@gmail.com>
2022-03-28 23:39:00 +0000

    "Wysłane automatycznie"

--- 2022-03-29Create Phone Number.py
+++ 2022-03-29Create Phone Number.py
@@ -1,5 +1,16 @@
 def create_phone_number(n):
-    first = "".join(n[:3])
-    return '('+first
+    first =str()
+    seccond =str()
+    three =str()
+
+    for i, num in enumerate(n):
+        if i <= 2:
+            first += str(num)
+        elif i <= 5:
+            seccond += str(num)
+        else: 
+            three += str(num)
+
+    return '('+first+') '+seccond+'-'+three
 
 print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
