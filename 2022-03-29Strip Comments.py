598770a3e0a4ef96a3d72f1e7f6c05bc4f51fd7c
SzybkiRabarbar <maksjoni36@gmail.com>
2022-03-29 16:15:00 +0000

    "Wysłane automatycznie"

---
+++ 2022-03-29Strip Comments.py
@@ -0,0 +1,7 @@
+def solution(string,markers):
+    s_lst=string.split('\n')
+    for mark in markers:
+        continue
+    return s_lst
+
+print(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]))
