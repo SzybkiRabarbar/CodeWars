a3666e35091ce5cdaed32994aa9d2e8ea5498f10
SzybkiRabarbar <maksjoni36@gmail.com>
2022-03-29 17:18:51 +0000

    "Wysłane automatycznie"

--- 2022-03-29Tree to list.py
+++
@@ -1,9 +0,0 @@
-class Node:
-    def __init__(self, data, child_nodes=None):
-        self.data = data
-        self.child_nodes = child_nodes
-
-def tree_to_list(tree_root):
-    pass
-
-print(Node(1, [Node(2, [Node(3), Node(4), Node(5)]), Node(3, [Node(7)])]), [1, 2, 3, 3, 4, 5, 7])
