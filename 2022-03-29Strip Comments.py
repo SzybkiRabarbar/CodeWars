2cb92cd3f9ab7a619772d9bc762875a68aa7a6ba
SzybkiRabarbar <maksjoni36@gmail.com>
2022-03-29 16:51:47 +0000

    "Wysłane automatycznie"

--- 2022-03-29Strip Comments.py
+++ 2022-03-29Strip Comments.py
@@ -1,7 +1,19 @@
-def solution(string,markers):
+'''
+https://www.codewars.com/kata/51c8e37cee245da6b40000bd/python
+'''
+def solution(string:str,markers):
     s_lst=string.split('\n')
-    for mark in markers:
-        continue
-    return s_lst
+    content = []
+    for line in s_lst:
+        find = False
+        for mark in markers:
+            if mark in line:
+                find = True
+                line = line.split(mark)[0]
+                if [mark2 for mark2 in markers if mark2 in line]: continue
+                content.append(line.strip())
+        if find: continue
+        content.append(line.strip())
+    return '\n'.join(content)
 
-print(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]))
+print(solution("apples, pears # and bananas\ngrapes\nbananas !#apples", ["#", "!"]))
