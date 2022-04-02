1e2a2da9790db093d90b72f2129c0c3919f15142
SzybkiRabarbar <maksjoni36@gmail.com>
2022-04-02 19:03:28 +0000

    Wysłane automatycznie

--- 2022-04/02Most frequently used words in a text.py
+++ 2022-04/02Most frequently used words in a text.py
@@ -1,5 +1,15 @@
 def top_3_words(text):
-    arr = text.split()
-    return arr
+    new_text = str()
+    for i, letter in enumerate(text):
+        if letter.isalpha():
+            new_text += letter
+        else:
+            if letter=="'" and (text[i-1].isalpha() or text[i+1].isalpha()):
+                new_text += "'"
+            new_text += ' '
+        
 
-print(top_3_words('Dzien dobry polsko'))
+
+    return new_text
+
+print(top_3_words("Dzie'n 'dobry ''' polsko"))
