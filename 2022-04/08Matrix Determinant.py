76781b9d426ba16aeb7d18c225913a9262678717
SzybkiRabarbar <maksjoni36@gmail.com>
2022-04-08 13:54:53 +0000

    Wysłane automatycznie

--- 2022-04/08Matrix Determinant.py
+++ 2022-04/08Matrix Determinant.py
@@ -1,5 +1,7 @@
+'''
+https://www.codewars.com/kata/52a382ee44408cea2500074c/python
+'''
 import numpy
 def determinant(matrix):
-    a = numpy.array(matrix)
-    return round(numpy.linalg.det(a))
+    return round(numpy.linalg.det(matrix))
 
