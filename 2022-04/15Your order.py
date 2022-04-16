3b8b2390adf8eeb628cbd87d801dd4df56cdc571
SzybkiRabarbar <maksjoni36@gmail.com>
2022-04-16 14:16:20 +0000

    Send by counter.py

--- 2022-04/15Your order.py
+++ 2022-04/15Your order.py
@@ -1,4 +1,13 @@
 #https://www.codewars.com/kata/55c45be3b2079eccff00010f/train/python
-def order(sentence):
-  sentence=[[w for w in word if w.isnumeric()],word for word in sentence]
-  return sentence
+def order(sentence:str):
+    sentence=[word for word in sentence.split()]
+    content =[]
+    for word in sentence[:]:
+        for letter in word:
+          if letter.isnumeric():
+            content.append([letter,word])
+            break
+    content.sort()
+    return ' '.join([x[1] for x in content])
+
+print(order('is2 Thi1s T4est 3a'))
