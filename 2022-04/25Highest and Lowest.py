1d1ba3ea59b90a7b167a2b68be97780d2cc54ee3
SzybkiRabarbar <maksjoni36@gmail.com>
2022-04-25 17:56:14 +0000

    Send by counter.py

--- 2022-04/25Highest and Lowest.py
+++ 2022-04/25Highest and Lowest.py
@@ -1,3 +1,6 @@
+'''
+https://www.codewars.com/kata/554b4ac871d6813a03000035/python
+'''
 def high_and_low(numbers):
     numbers=[int(n) for n in numbers.split()]
     return f'{max(numbers)} {min(numbers)}'
