a456633bc7606a3607470fda5699fdb678b60127
SzybkiRabarbar <maksjoni36@gmail.com>
2022-04-30 15:20:06 +0000

    Send by counter.py

--- 2022-04/30Baby shark lyrics generator.py
+++ 2022-04/30Baby shark lyrics generator.py
@@ -1,13 +1,12 @@
-SongText={'doo':'doo doo doo doo doo doo',
-          'baby':'Baby shark',
-          'mommy':'Mommy shark',
-          'daddy':'Daddy shark',
-          'grandma':'Grandma shark',
-          'grandpa':'Grandpa shark',
-          'bloodlust':"Let's go hunt"}
+'''
+https://www.codewars.com/kata/5d076515e102162ac0dc514e/python
+'''
+t=['Baby shark','Mommy shark','Daddy shark','Grandma shark','Grandpa shark',"Let's go hunt",'doo doo doo doo doo doo']
 def baby_shark_lyrics():
-    result=str()
-    for i in range(3):
-        result+=f"{SongText.get('baby')}"
+    r=str()
+    for i in range(6):
+        for _ in range(3):
+            r+=f"{t[i]}, {t[6]}\n"
+        r+=f"{t[i]}!\n"
+    return f"{r}Run away,…"
 
-print(baby_shark_lyrics())
