f975bc2a91664af08d71fa4443c22f3800cec167
SzybkiRabarbar <maksjoni36@gmail.com>
2022-05-07 16:00:20 +0000

    Send by counter.py

--- 2022-05/07Formula 1 Race.py
+++ 2022-05/07Formula 1 Race.py
@@ -11,6 +11,3 @@ def champion_rank(verstappen: int, events: str) -> int:
         if event == 'I':
             pos.pop(pos.index(pilot)) 
     return pos.index(str(verstappen))+1
-
-i1, i2 = 20, "9 O 17 O 9 O 12 O 2 O 12 O 9 O 1 O 5 O 12 O 17 O 20 O 16 O 7 O 2 O 8 O 16 O 14 O 3 I 14 O 11 O 16 O 1 I 13 O 8 O 14 O 5 I 12 O 4 O"
-print(formula1(i1,i2))
