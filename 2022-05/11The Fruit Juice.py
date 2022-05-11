c004af69693554018a75b11c92b8d0fa2ff1e667
SzybkiRabarbar <maksjoni36@gmail.com>
2022-05-11 14:08:17 +0000

    Send by counter.py

--- 2022-05/11The Fruit Juice.py
+++ 2022-05/11The Fruit Juice.py
@@ -1,24 +1,23 @@
 #https://www.codewars.com/kata/5264603df227072e6500006d/python
 class Jar():
     def __init__(self):
-        self.dct=dict()
-        pass
-    
+        self.dct = dict()
+
     def add (self, amount, kind):
         if kind in self.dct:
-            self.dct[kind] = self.dct[kind]+amount
+            self.dct[kind] += amount
         else:
             self.dct[kind] = amount
     
     def pour_out (self, amount):
-        amount = amount/len(self.dct)
+        proportion = amount / self.get_total_amount()
         for key, value in self.dct.items():
-            self.dct[key] = value-amount
+            self.dct[key] = value * (1 - proportion)
     
     def get_total_amount(self):
-        return sum([value for _, value in self.dct.items()])
+        return sum(self.dct.values()) 
     
     def get_concentration(self, kind):
         if kind in self.dct:
-            return self.dct.get(kind)/self.get_total_amount()
+            return self.dct[kind] / self.get_total_amount()
         return 0
