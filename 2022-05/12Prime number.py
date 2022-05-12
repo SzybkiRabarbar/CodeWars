5384881c543042e2fcae713d4df57841ac5a7abb
SzybkiRabarbar <maksjoni36@gmail.com>
2022-05-12 01:38:34 +0000

    Send by counter.py

---
+++ 2022-05/12Prime number.py
@@ -0,0 +1,8 @@
+def is_prime(n):
+    if n<2: return False
+    result = set()
+    for i in range(1, int(n**0.5)+1):
+        if n % i == 0:
+            result.add(i)
+            result.add(n//i)
+    return len(result)==2
